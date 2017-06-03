162c1f27715d019ea7bcf80e9705a09ba1678fda
David Dietrich <grayarrow@gmail.com>
2017-06-03 12:11:45 -0700

    Upgraded to Python 3.

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
 
 # load config from a JSON file (or anything outputting a python dictionary)
-with open("dejavu.cnf.SAMPLE") as f:
+with open("dejavu.cnf") as f:
     config = json.load(f)
 
 if __name__ == '__main__':
@@ -19,17 +19,17 @@ if __name__ == '__main__':
 
 	# Recognize audio from a file
 	song = djv.recognize(FileRecognizer, "mp3/Sean-Fournier--Falling-For-You.mp3")
-	print "From file we recognized: %s\n" % song
+	print("From file we recognized: {}\n".format(song))
 
 	# Or recognize audio from your microphone for `secs` seconds
-	secs = 5
-	song = djv.recognize(MicrophoneRecognizer, seconds=secs)
-	if song is None:
-		print "Nothing recognized -- did you play the song out loud so your mic could hear it? :)"
-	else:
-		print "From mic with %d seconds we recognized: %s\n" % (secs, song)
+#	secs = 5
+#	song = djv.recognize(MicrophoneRecognizer, seconds=secs)
+#	if song is None:
+#		print("Nothing recognized -- did you play the song out loud so your mic could hear it? :)")
+#	else:
+#		print("From mic with %d seconds we recognized: %s\n" % (secs, song))
 
 	# Or use a recognizer without the shortcut, in anyway you would like
-	recognizer = FileRecognizer(djv)
-	song = recognizer.recognize_file("mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
-	print "No shortcut, we recognized: %s\n" % song
+#	recognizer = FileRecognizer(djv)
+#	song = recognizer.recognize_file("mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
+#	print("No shortcut, we recognized: {}\n".format(song))
