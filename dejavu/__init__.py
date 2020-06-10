d7baace3d1178846d3b50865a9511d1a676e8833
seunghoon.baek <seunghoon.baek@linecorp.com>
2020-06-10 21:18:24 +0900

    Fix runtime error that is not matched an argument type.

--- dejavu/__init__.py
+++ dejavu/__init__.py
@@ -133,11 +133,7 @@ class Dejavu:
         if song_hash in self.songhashes_set:
             print(f"{song_name} already fingerprinted, continuing...")
         else:
-            song_name, hashes, file_hash = Dejavu._fingerprint_worker(
-                file_path,
-                self.limit,
-                song_name=song_name
-            )
+            song_name, hashes, file_hash = Dejavu._fingerprint_worker((file_path, self.limit))
             sid = self.db.insert_song(song_name, file_hash)
 
             self.db.insert_hashes(sid, hashes)
