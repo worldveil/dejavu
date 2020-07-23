be266b9426d29eaab2868d6ff7df7657df089e06
Raed Atoui <raed.atoui@gmail.com>
2020-07-22 23:05:29 -0400

    add missing total_hashes

--- dejavu/__init__.py
+++ dejavu/__init__.py
@@ -138,7 +138,7 @@ class Dejavu:
                 self.limit,
                 song_name=song_name
             )
-            sid = self.db.insert_song(song_name, file_hash)
+            sid = self.db.insert_song(song_name, file_hash, len(hashes))
 
             self.db.insert_hashes(sid, hashes)
             self.db.set_song_fingerprinted(sid)
