702866401de998ec387130b71fa9670b3d4d67d5
Qays <whosqays@gmail.com>
2020-10-13 14:59:19 +0800

    Update test_dejavu.sh

--- test_dejavu.sh
+++ test_dejavu.sh
@@ -16,10 +16,10 @@ python dejavu.py -f ./mp3/ mp3
 # away from start or end, sampling with random seed = 42, and finally 
 # store results in ./results and log to dejavu-test.log
 python run_tests.py \
-	--secs 5 \
-	--temp ./temp_audio \
-	--log-file ./results/dejavu-test.log \
-	--padding 8 \
-	--seed 42 \
-	--results ./results \
+	-sec 5 \
+	-temp ./temp_audio \
+	-lf ./results/dejavu-test.log \
+	-pad 8 \
+	-sd 42 \
+	-res ./results \
 	./mp3
